4b6c8e827c912b31a3e22606c754190740eafe9a
Lucas Soto <lucasrafsoto@gmail.com>
2025-03-17 00:09:52 -0700

    changing port number

--- src/server/index.ts
+++ src/server/index.ts
@@ -8,7 +8,7 @@ import cookieParser from "cookie-parser";
 import rootRoutes from "./routes/root";
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(morgan("dev"));
 app.use(express.json());
