d18c7f8a0ab16c031bec874b2e2d44bfdbe2c021
Lucas Soto <lucasrafsoto@gmail.com>
2025-03-16 23:55:42 -0700

    testing auto husky

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,18 +1,21 @@
-import * as path from 'path';
+import * as path from "path";
 
-import express from 'express';
-import httpErrors from 'http-errors';
+import express from "express";
+import httpErrors from "http-errors";
+import morgan from "morgan";
+import cookieParser from "cookie-parser";
 
-import rootRoutes from './routes/root';
-import { timeMiddleware } from './middleware/time';
+import rootRoutes from "./routes/root";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(timeMiddleware);
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.use(express.static(path.join(process.cwd(), "public")));
-
 app.set("views", path.join(process.cwd(), "views"));
 app.set("view engine", "ejs");
 
@@ -20,9 +23,9 @@ app.use("/", rootRoutes);
 app.use("/test", () => {});
 
 app.use((_request, _response, next) => {
-    next(httpErrors(404));
+  next(httpErrors(404));
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-})
+  console.log(`Server is running on port ${PORT}`);
+});
