62d1b11fd4dab5f92304d86cd0487252d5a365d6
Lucas Soto <lucasrafsoto@gmail.com>
2025-03-17 00:04:47 -0700

    Husky added

--- src/server/middleware/time.ts
+++ src/server/middleware/time.ts
@@ -1,10 +1,14 @@
-import { NextFunction, Request, Response } from 'express';
+import { NextFunction, Request, Response } from "express";
 
 // example of a middleware function that will be used for user requests
-const timeMiddleware = (_request: Request, _response: Response, next: NextFunction) => {
-    console.log(`Request made at ${new Date().toISOString()}`);
+const timeMiddleware = (
+  _request: Request,
+  _response: Response,
+  next: NextFunction,
+) => {
+  console.log(`Request made at ${new Date().toISOString()}`);
 
-    next();
+  next();
 };
 
 export { timeMiddleware };
--- src/server/routes/root.ts
+++ src/server/routes/root.ts
@@ -1,13 +1,13 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
 router.get("/", (request, response, next) => {
-    // replace vars with db calls
-    const title = "Jarl's site";
-    const message = "Welcome to my site!";
+  // replace vars with db calls
+  const title = "Jarl's site";
+  const message = "Welcome to my site!";
 
-    response.render("root", { message });
-})
+  response.render("root", { message });
+});
 
 export default router;
