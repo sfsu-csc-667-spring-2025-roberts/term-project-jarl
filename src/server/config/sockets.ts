c0a60a64694062407441974304795b11cc8645fa
Mikko <realmiks27@gmail.com>
2025-05-19 08:27:23 -0700

    add ritesh's code for card UI creation, table, players, game chat UI, and get game chat to work

--- src/server/config/sockets.ts
+++ src/server/config/sockets.ts
@@ -1,6 +1,19 @@
 import type { Express } from "express";
-import type { Server } from "socket.io";
+import type { Server, Socket } from "socket.io";
 import { sessionMiddleware } from "./session";
+import db from "../db/connection";
+
+const sendOneCardToClient = async (socket: Socket) => {
+  try {
+    const result = await db.one(
+      "SELECT * FROM cards ORDER BY RANDOM() LIMIT 1",
+    );
+    socket.emit("dealCard", result);
+    console.log("Sent card to client:", result);
+  } catch (err) {
+    console.error("Error sending card:", err);
+  }
+};
 
 const configureSockets = (io: Server, app: Express) => {
   app.set("io", io);
@@ -9,12 +22,48 @@ const configureSockets = (io: Server, app: Express) => {
 
   io.on("connection", (socket) => {
     // @ts-ignore
-    const { id, user } = socket.request.session;
+    const { id, user, game_id } = socket.request.session;
 
     console.log(
       `User [${user.user_id}] connected: ${user.email} with session id ${id}`,
     );
     socket.join(user.id);
+
+    // testing purposes
+    // sendOneCardToClient(socket);
+
+    socket.on("gameChatMessage", async (message: string) => {
+      try {
+        await db.none(
+          `
+          INSERT INTO messages (content, author, game_player_id, "isLobby")
+          VALUES (
+            $1,
+            $2,
+            (
+              SELECT game_player_id
+              FROM "gamePlayers"
+              WHERE user_id = $2 AND game_id = $3
+            ),
+            false
+          )
+        `,
+          [message, user.user_id, game_id],
+        );
+
+        const userResult = await db.one(
+          "SELECT username FROM users WHERE user_id = $1",
+          [user.user_id],
+        );
+
+        io.emit("gameChatMessage", {
+          user: userResult.username,
+          content: message,
+        });
+      } catch (err) {
+        console.error("Error handling game chat message:", err);
+      }
+    });
 
     socket.on("disconnect", () => {
       console.log(
